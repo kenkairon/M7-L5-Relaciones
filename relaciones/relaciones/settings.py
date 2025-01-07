2e77734163e5880eaed3887c8d97a86864ee6099
kenkairon <ingeinfo2006@gmail.com>
2025-01-07 20:28:58 -0300

    Agregar el trabajo en clases

--- relaciones/relaciones/settings.py
+++ relaciones/relaciones/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'relaciones.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
